b0b412db263feb9ef7858f8656916a2c42b78b34
AstroTaka <53452427+AstroTaka@users.noreply.github.com>
2020-04-21 04:42:03 +0900

    Update formatDate.ts

--- utils/formatDate.ts
+++ utils/formatDate.ts
@@ -17,7 +17,7 @@ export const convertDateToDataViewDisplayText =(dateString: string): string => {
   return dayjs(dateString).format('YYYY/MM/DD HH:mm')
 }
 
-export const convertLastDateDisplayText =(dateString1: string, dateString2: string): string => {
+export default (dateString1: string, dateString2: string) => {
   if(dayjs(dateString1).isAfter(dayjs(dateString2))){
     return dayjs(dateString1).format('YYYY/MM/DD HH:mm')
   }
