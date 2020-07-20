eb1e90ba0d6e2ce2412dceac3ab934c0ce753f23
AstroTaka <53452427+AstroTaka@users.noreply.github.com>
2020-07-20 13:42:11 +0900

    Update formatTable.ts

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -5,7 +5,7 @@ const headers = [
 //  { text: '居住地', value: '居住地' },
   { text: '年代', value: '年代' },
   { text: '性別', value: '性別' },
-  { text: '状況', value: '退院'}
+  { text: '状況', value: '状況'}
 ]
 
 type DataType = {
@@ -13,7 +13,7 @@ type DataType = {
   居住地: string | null
   年代: string | null
   性別: '男性' | '女性'
-  退院: '◯' | null
+  状況: '◯' | null
   [key: string]: any
 }
 
@@ -22,7 +22,7 @@ type TableDataType = {
   居住地: DataType['居住地']
   年代: DataType['年代']
   性別: DataType['性別'] | '不明'
-  退院: DataType['退院']
+  状況: DataType['状況']
 }
 
 type TableDateType = {
@@ -41,7 +41,7 @@ export default (data: DataType[]) => {
       居住地: d['居住地'] ?? '不明',
       年代: d['年代'] ?? '不明',
       性別: d['性別'] ?? '不明',
-      退院: d['退院']
+      状況: d['状況']
     }
     tableDate.datasets.push(TableRow)
   })
